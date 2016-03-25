49a9b0a5e8d711d8b92bdcf04c6393ee68f7a5b4
KristianNaranjo <kristiannaranjo@gmail.com>
2016-03-24 23:18:19 -0700

    It works now!

--- src/CreateRepo.java
+++ src/CreateRepo.java
@@ -1,101 +1,134 @@
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Date;
 import java.util.Scanner;
-
+import java.io.PrintWriter;
 public class CreateRepo {
-
+	private static PrintWriter out = null;
 	public static void main(String[] args) {
 		System.out.print("\nr343> ");
 		Scanner in = new Scanner(System.in);
 		String input = in.nextLine();
-		String [] user = input.split(" ");
-		String cmd = user[0];
-		String dirName = user[1];
-		String src = user[2];
-		String target = user[3];
-		
-		File theDir = new File(target+"/"+dirName);
-
-		// if the directory does not exist, create it
-		if (!theDir.exists()) {
-		    System.out.println("creating "+dirName);
-		    boolean result = false;
-
-		    try{
-		        theDir.mkdir();
-		        result = true;
-		    } 
-		    catch(SecurityException se){
-		        //handle it
-		    }        
-		    if(result) {    
-		        System.out.println("DIR created");  
-		    }
+		if (input.split(" ").length == 4){
+			String [] user = input.split(" ");
+			String cmd = user[0];
+			String repo = user[1];
+			File src = new File(user[2]);
+			File target = new File(user[3]);
+			if (cmd.equals("create") && repo.equals("repo")){
+				if(src.exists() && target.exists()){
+					initialize(user[2], user[3]);
+					System.out.print("repo created");
+				}
+				else System.out.print("Either source folder or target folder does not exist.");
+			}
 		}
-		
-		File subDir = new File(target+"/"+dirName+"/manifests");
-
-		// if the directory does not exist, create it
-		if (!subDir.exists()) {
-		    System.out.println("creating manifests");
-		    boolean result = false;
-
-		    try{
-		        subDir.mkdir();
-		        result = true;
-		    } 
-		    catch(SecurityException se){
-		        //handle it
-		    }        
-		    if(result) {    
-		        System.out.println("manifests created");  
-		    }
-		}
-		System.out.println(subDir.getPath());
-		// access target folder specified by user
-		File srcDir = new File(src);
-		
-		// duplicate tree into repo
-		File dupTree = new File(target+"/"+dirName+"/"+src);
-
-		// if the directory does not exist, create it
-		if (!dupTree.exists()) {
-		    System.out.println("creating duplicate of ptree into repo");
-		    boolean result = false;
-		    try{
-		        dupTree.mkdir();
-		        result = true;
-		    } 
-		    catch(SecurityException se){
-		    }        
-		    if(result) {    
-		        System.out.println("duplicate ptree created");  
-		    }
-		}
-		String repoTreePath = target+"/"+dirName+"/"+src;
-		copyTree(srcDir,repoTreePath);
+		if(!input.equals("exit"))
+			main(args);
+		in.close();
+	}
+	
+	static void initialize(String src, String target){
+		String repo = "repo343";
+		// create repo directory in target directory
+				File theDir = new File(target+"/"+repo);
+				createDir(theDir);
+				
+				// create manifest directory
+				File manifest = new File(target+"/"+repo+"/manifests");
+				createDir(manifest);
+				
+				// save manifest path
+				String manPath = target+"/"+repo+"/manifests/"+getDate().replace(':', '-')+".txt";
+				try {
+					out = new PrintWriter(manPath);
+				} catch (FileNotFoundException e1) {
+					e1.printStackTrace();
+				}
+				// access target folder specified by user
+				File srcDir = new File(src);
+				
+				// create tree directory in repo
+				File dupTree = new File(target+"/"+repo+"/"+src);
+				createDir(dupTree);
+				
+				// duplicate tree inside repo
+				String repoTreePath = target+"/"+repo+"/"+src;
+				copyTree(srcDir,repoTreePath);
 	}
 
 	static void copyTree(File folder,String path){
+		PrintWriter artOut = null;
+		Scanner artIn = null;
+		String artID = "";
+		String leafPath = "";
+		
 		for (final File fileEntry : folder.listFiles()) {
+			leafPath = path+"/"+fileEntry.getName();
 	        if (fileEntry.isDirectory()) {
-	            copyTree(fileEntry,path);
+	        	// add 
+	        	createDir(new File(leafPath));
+	            copyTree(fileEntry,leafPath);
 	        } else {
-	        	String leafPath = path+"/"+fileEntry.getName();
 	        	File leafFolder = new File(leafPath);
-	        	if (!leafFolder.exists()) {
-	    		    System.out.println("creating leaf folder");
-	    		    boolean result = false;
-	    		    try{
-	    		        leafFolder.mkdir();
-	    		        result = true;
-	    		    } 
-	    		    catch(SecurityException se){
-	    		    }        
-	    		    if(result) {    
-	    		        System.out.println("leaf folder created");  
-	    		    }
-	    		}
+	        	createDir(leafFolder);
+		        artID = Integer.toString(getCheckSum(fileEntry));
+		        // create artifact file
+		    	createFile(leafPath+"/"+artID+".txt");
+		    	// output artifact path to manifest
+		    	out.println(leafPath+"/"+artID+".txt\n");
+		    	out.flush();
+		    	// write to artifact
+		    	try {
+		    		// output to artifact
+					artOut = new PrintWriter(leafPath+"/"+artID+".txt");
+					// input from file in src
+					artIn = new Scanner(fileEntry);
+					while(artIn.hasNextLine())
+						artOut.println(artIn.nextLine());
+					artOut.flush();
+				} catch (FileNotFoundException e) {
+					e.printStackTrace();
+				}
 	        }
 	    }
 	}
+	static int getCheckSum(File file){
+		// use size of file to get AID
+		long bytes = file.length();
+		// get number between 0 and 255
+		int artID = (int) (bytes % 256);
+		return artID;
+	}
+	
+	static void createFile(String path){
+		File file = new File(path);
+		// if the directory does not exist, create it
+		if (!file.exists()) {
+		    try{
+		    	file.createNewFile();
+		    } 
+		    catch(SecurityException se){
+		    } catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	static String getDate(){
+		Date date = new Date();
+	    return date.toString();
+	}
+	
+	static void createDir(File theDir){
+		// if the directory does not exist, create it
+		if (!theDir.exists()) {
+		    try{
+		        theDir.mkdir();
+		    } 
+		    catch(SecurityException se){
+		    }        
+		}
+	}
+
 }
